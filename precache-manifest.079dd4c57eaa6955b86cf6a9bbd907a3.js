b39dcf745c4d2b803b9d5c10fca9972b222bc90c
Moses Surumen <moses.susrumen@gmail.com>
2019-01-23 20:19:57 -0800

    Updates

---
+++ precache-manifest.079dd4c57eaa6955b86cf6a9bbd907a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73fbfc50a4111cd4d689",
+    "url": "/somalab/static/js/runtime~main.73fbfc50.js"
+  },
+  {
+    "revision": "5ce1eddd39cd0eae7510",
+    "url": "/somalab/static/js/main.5ce1eddd.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/somalab/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5ce1eddd39cd0eae7510",
+    "url": "/somalab/static/css/main.b3a2a1c3.chunk.css"
+  },
+  {
+    "revision": "8b860bb1a09d0ac8525b64cea8429854",
+    "url": "/somalab/index.html"
+  }
+];
